cef610899da7faac852a6b07baa77c8b77526b56
Slawek Kaczorowski <slawkacz@gmail.com>
2015-03-12 20:15:41 +0100

    Added  promises in Photos Service

--- www/js/services.js
+++ www/js/services.js
@@ -1,7 +1,11 @@
-angular.module('gestureApp.services', []).factory('Photos', function($http, API_URL) {
+angular.module('gestureApp.services', []).factory('Photos', function($http, API_URL, $q) {
     return {
         getPhotos: function() {
-            return $http.get(API_URL + '&kimlimit=:limit&kimoffset=:offset');
+            var deffered = $q.defer();
+            $http.get(API_URL + '&kimlimit=100&kimoffset=0').then(function(res) {
+                deffered.resolve(res.data.results.Images);
+            }, deffered.reject);
+            return deffered.promise;
         }
     }
 });
