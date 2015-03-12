0d4fcd7183fa794d0bd89e9d57f393f709e661ba
Slawek Kaczorowski <slawkacz@gmail.com>
2015-03-12 20:46:28 +0100

    * Removed scrolling content * Added swipe left/right events

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,4 +5,10 @@ angular.module('gestureApp.controllers', []).controller('PhotosCtrl', function(P
         $scope.photos = images;
         $scope.photo = $scope.photos[0];
     });
+    $scope.onSwipeLeft = function(){
+    	console.log('left')
+    };
+    $scope.onSwipeRight = function(){
+    	console.log('right')
+    };
 });
