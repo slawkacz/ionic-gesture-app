732aed40394e68d9c581ee0c00aac9fe89663930
Slawek Kaczorowski <slawkacz@gmail.com>
2015-03-12 20:21:50 +0100

    Showing image on screen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,9 @@
 angular.module('gestureApp.controllers',[]).controller('PhotosCtrl',function(Photos,$scope){
 	$scope.photos = [];
+	$scope.photo;
 	Photos.getPhotos().then(function(images){
 		$scope.photos = images;
+		$scope.photo = $scope.photos[0];
 	});
+
 });
