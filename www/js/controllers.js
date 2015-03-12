728065e25b2790b00815ce0fcaf362411ff56b7a
Slawek Kaczorowski <slawkacz@gmail.com>
2015-03-12 20:39:48 +0100

    Dont bother just formatting code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,8 @@
-angular.module('gestureApp.controllers',[]).controller('PhotosCtrl',function(Photos,$scope){
-	$scope.photos = [];
-	$scope.photo;
-	Photos.getPhotos().then(function(images){
-		$scope.photos = images;
-		$scope.photo = $scope.photos[0];
-	});
-
+angular.module('gestureApp.controllers', []).controller('PhotosCtrl', function(Photos, $scope) {
+    $scope.photos = [];
+    $scope.photo;
+    Photos.getPhotos().then(function(images) {
+        $scope.photos = images;
+        $scope.photo = $scope.photos[0];
+    });
 });
--- www/js/interceptors.js
+++ www/js/interceptors.js
@@ -1,32 +1,33 @@
-angular.module('gestureApp').factory('httpInterceptor', function ($q, $rootScope, $log) {
-        var numLoadings = 0;
-        return {
-            request: function (config) {
-                numLoadings++;
-                $rootScope.$broadcast('loading:show');
-                return config || $q.when(config)
-            },
-            response: function (response) {
-                if ((--numLoadings) === 0) {
-                    $rootScope.$broadcast('loading:hide');
-                }
-                return response || $q.when(response);
-            },
-            responseError: function (response) {
-                if (!(--numLoadings)) {
-                    $rootScope.$broadcast('loading:hide');
-                }
-                return $q.reject(response);
+angular.module('gestureApp').factory('httpInterceptor', function($q, $rootScope, $log) {
+    var numLoadings = 0;
+    return {
+        request: function(config) {
+            numLoadings++;
+            $rootScope.$broadcast('loading:show');
+            return config || $q.when(config)
+        },
+        response: function(response) {
+            if ((--numLoadings) === 0) {
+                $rootScope.$broadcast('loading:hide');
             }
-        };
-    }).config(function ($httpProvider) {
-        $httpProvider.interceptors.push('httpInterceptor');
-    }).run(function ($rootScope, $ionicLoading) {
-        $rootScope.$on('loading:show', function () {
-            $ionicLoading.show({template: 'Loading...'})
+            return response || $q.when(response);
+        },
+        responseError: function(response) {
+            if (!(--numLoadings)) {
+                $rootScope.$broadcast('loading:hide');
+            }
+            return $q.reject(response);
+        }
+    };
+}).config(function($httpProvider) {
+    $httpProvider.interceptors.push('httpInterceptor');
+}).run(function($rootScope, $ionicLoading) {
+    $rootScope.$on('loading:show', function() {
+        $ionicLoading.show({
+            template: 'Loading...'
         })
-
-        $rootScope.$on('loading:hide', function () {
-            $ionicLoading.hide()
-        })
-    });;
+    })
+    $rootScope.$on('loading:hide', function() {
+        $ionicLoading.hide()
+    })
+});
