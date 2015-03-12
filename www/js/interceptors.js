72553a53df0750ce2307b267189ea9a94ba2339c
Slawek Kaczorowski <slawkacz@gmail.com>
2015-03-12 20:32:46 +0100

    Added loading http interceptor

---
+++ www/js/interceptors.js
@@ -0,0 +1,22 @@
+angular.module('gestureApp').factory('httpInterceptor', function ($q, $rootScope, $log) {
+        var numLoadings = 0;
+        return {
+            request: function (config) {
+                numLoadings++;
+                $rootScope.$broadcast('loading:show');
+                return config || $q.when(config)
+            },
+            response: function (response) {
+                if ((--numLoadings) === 0) {
+                    $rootScope.$broadcast('loading:hide');
+                }
+                return response || $q.when(response);
+            },
+            responseError: function (response) {
+                if (!(--numLoadings)) {
+                    $rootScope.$broadcast('loading:hide');
+                }
+                return $q.reject(response);
+            }
+        };
+    });
