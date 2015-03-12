b646d60370d5282a9fca2287b2ae45e408ebcfed
Slawek Kaczorowski <slawkacz@gmail.com>
2015-03-12 20:17:49 +0100

    * Added Photo controller * Fetching Photos from Kimonolabs API via Photos Service * Showing $scope.photos array in body

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,1 +1,6 @@
-angular.module('gestureApp.controllers',[]);
+angular.module('gestureApp.controllers',[]).controller('PhotosCtrl',function(Photos,$scope){
+	$scope.photos = [];
+	Photos.getPhotos().then(function(images){
+		$scope.photos = images;
+	});
+});
