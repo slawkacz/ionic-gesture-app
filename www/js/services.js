c03b04b1ce208fb5552d5f73d856ebf7a3d1576b
Slawek Kaczorowski <slawkacz@gmail.com>
2015-03-16 20:23:54 +0100

    Added  and Storage services

--- www/js/services.js
+++ www/js/services.js
@@ -1,4 +1,25 @@
-angular.module('gestureApp.services', []).factory('Photos', function($http, API_URL, $q) {
+angular.module('gestureApp.services', [])
+.factory('$localstorage', ['$window', function($window) {
+  return {
+    set: function(key, value) {
+      $window.localStorage[key] = value;
+    },
+    get: function(key, defaultValue) {
+      return $window.localStorage[key] || defaultValue;
+    },
+    setObject: function(key, value) {
+      $window.localStorage[key] = JSON.stringify(value);
+    },
+    getObject: function(key) {
+      return JSON.parse($window.localStorage[key] || '{}');
+    }
+  }
+}]).factory('Storage',function($window){
+	return {
+		
+	}
+})
+.factory('Photos', function($http, API_URL, Storage, $q) {
     return {
         getPhotos: function() {
             var deffered = $q.defer();
